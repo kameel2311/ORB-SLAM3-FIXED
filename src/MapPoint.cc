dd734971cff41a2be7009e65624cb8ffa4551af4
kameel.amareen <kameel.amareen@yahoo.com>
2024-05-16 13:56:34 +0200

    Applied Map Saving Code Patch

--- src/MapPoint.cc
+++ src/MapPoint.cc
@@ -569,6 +569,36 @@ void MapPoint::UpdateMap(Map* pMap)
     mpMap = pMap;
 }
 
+// void MapPoint::PreSave(set<KeyFrame*>& spKF,set<MapPoint*>& spMP)
+// {
+//     mBackupReplacedId = -1;
+//     if(mpReplaced && spMP.find(mpReplaced) != spMP.end())
+//         mBackupReplacedId = mpReplaced->mnId;
+
+//     mBackupObservationsId1.clear();
+//     mBackupObservationsId2.clear();
+//     // Save the id and position in each KF who view it
+//     for(std::map<KeyFrame*,std::tuple<int,int> >::const_iterator it = mObservations.begin(), end = mObservations.end(); it != end; ++it)
+//     {
+//         KeyFrame* pKFi = it->first;
+//         if(spKF.find(pKFi) != spKF.end())
+//         {
+//             mBackupObservationsId1[it->first->mnId] = get<0>(it->second);
+//             mBackupObservationsId2[it->first->mnId] = get<1>(it->second);
+//         }
+//         else
+//         {
+//             EraseObservation(pKFi);
+//         }
+//     }
+
+//     // Save the id of the reference KF
+//     if(spKF.find(mpRefKF) != spKF.end())
+//     {
+//         mBackupRefKFId = mpRefKF->mnId;
+//     }
+// }
+
 void MapPoint::PreSave(set<KeyFrame*>& spKF,set<MapPoint*>& spMP)
 {
     mBackupReplacedId = -1;
@@ -578,7 +608,10 @@ void MapPoint::PreSave(set<KeyFrame*>& spKF,set<MapPoint*>& spMP)
     mBackupObservationsId1.clear();
     mBackupObservationsId2.clear();
     // Save the id and position in each KF who view it
-    for(std::map<KeyFrame*,std::tuple<int,int> >::const_iterator it = mObservations.begin(), end = mObservations.end(); it != end; ++it)
+    std::map<KeyFrame*,std::tuple<int,int> > tmp_mObservations;
+    tmp_mObservations.insert(mObservations.begin(), mObservations.end());
+
+    for(std::map<KeyFrame*,std::tuple<int,int> >::const_iterator it = tmp_mObservations.begin(), end = tmp_mObservations.end(); it != end; ++it)
     {
         KeyFrame* pKFi = it->first;
         if(spKF.find(pKFi) != spKF.end())
@@ -592,8 +625,7 @@ void MapPoint::PreSave(set<KeyFrame*>& spKF,set<MapPoint*>& spMP)
         }
     }
 
-    // Save the id of the reference KF
-    if(spKF.find(mpRefKF) != spKF.end())
+    // Save the id of the reference KFif(spKF.find(mpRefKF) != spKF.end())
     {
         mBackupRefKFId = mpRefKF->mnId;
     }
